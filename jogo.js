b224dcdc97fa9ddbf5a58800fce0f52d3e13a359
Lucas Miranda <106752991+lucaslindo123@users.noreply.github.com>
2023-10-27 10:48:41 -0300

    jogo da velha

---
+++ jogo.js
@@ -0,0 +1,9 @@
+//Seleciona todos os elementos com a classe "celula" e os armazenamentos
+const celulas = document.querySelectorAll (".celula");
+
+let checarTurno = true;
+
+let turno;
+
+const JOGADOR_X = "X";
+const JOGADOR_O = "O";
